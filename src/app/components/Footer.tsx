43f9a69adcf46cbe7b4761ed2bb3d119627753f6
faris <farisriski999@gmail.com>
2023-10-25 00:59:56 +0700

    penambahan header, tapi masih ada error

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,8 +4,7 @@ import socmed from "../../../__mocks__/socmed.json"
 
 const Footer = () => {
     return <footer className="w-full text-gray-700 bg-gray-100 body-font">
-        <div
-            className="container flex flex-col flex-wrap px-5 py-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
+        <div className="container flex flex-col flex-wrap px-5 py-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
             <div className="flex-shrink-0 w-64 mx-auto text-center md:mx-0 md:text-left">
                 <a className="flex items-center justify-center font-medium text-gray-900 title-font md:justify-start">
                     <svg className="w-auto h-5 text-gray-900 fill-current" viewBox="0 0 202 69"
@@ -22,14 +21,7 @@ const Footer = () => {
                                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                             </svg>
                         </a>
-                        {/* <a className="ml-3 text-gray-500 cursor-pointer hover:text-gray-700">
-                            <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                className="w-5 h-5" viewBox="0 0 24 24">
-                                <path
-                                    d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z">
-                                </path>
-                            </svg>
-                        </a> */}
+
                         <a className="ml-3 text-gray-500 cursor-pointer hover:text-gray-700">
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                 stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
@@ -51,19 +43,7 @@ const Footer = () => {
             </div>
 
             <div className="flex flex-wrap flex-grow mt-10 -mb-10 text-center md:pl-20 md:mt-0 md:text-left">
-                <div class="w-full px-4 lg:w-1/4 md:w-1/2">
-                    <h2 class="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">About</h2>
-                    <nav class="mb-10 list-none">
-                        <li class="mt-3">
-                            <a class="text-gray-500 cursor-pointer hover:text-gray-900">Company</a>
-                        </li>
-                        <li class="mt-3">
-                            <a class="text-gray-500 cursor-pointer hover:text-gray-900">Careers</a>
-                        </li>
-                        <li class="mt-3">
-                            <a class="text-gray-500 cursor-pointer hover:text-gray-900">Blog</a>
-                        </li>
-                    </nav>
+                <div className="w-full px-4 lg:w-1/4 md:w-1/2">
                 </div>
 
                 <div className="w-full px-4 lg:w-1/4 md:w-1/2">
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,55 @@
+import services from "../../../__mocks__/services.json"
+import products from "../../../__mocks__/products.json"
+import navIcon1 from '../../../public/a.jpeg';
+
 const Header = () => {
     return <header>
-        ini header guys
-
-        project stomil
+        <nav className="navbar navbar-expand-lg navbar-light fixed w-full horizontal-nav">
+            <div className="container">
+                <div className="flex items-center justify-between">
+                    <a className="navbar-brand" href="index_1.html">
+                        <img src={navIcon1} className="mr-2 w-[130px]" alt="logo" />
+                    </a>
+                    <button className="navbar-toggler hidden text-end mobile-menu-button">
+                        <span className="navbar-toggler-icon">
+                            <i className="ti-menu icon-align-right"></i>
+                        </span>
+                    </button>
+                    <div className="navbar-collapse flex-1 mobile-menu hidden min-[991px]:block" id="navbarNav">
+                        <ul className="navbar-nav justify-end horizontal-menu">
+                            <li className="scroll dropdown active group/menu">
+                                <a href="javaScript:void();" className="dropdown-toggle"><span>Home</span></a>
+                            </li>
+                            <li className="scroll dropdown group/menu">
+                                <a href="javaScript:void();" className="dropdown-toggle"><span>About Us</span></a>
+                            </li> 
+                            <li className="scroll dropdown group/menu">
+                                <a href="javaScript:void();" className="dropdown-toggle"><span>Products</span></a>
+                                <ul className="dropdown-menu hidden group-hover/menu:block">
+                                    {products.products.map((item, i) => (
+                                        <li key={i}> <a href={item.href}> {item.title} Product</a></li>
+                                    ))}
+                                </ul>
+                            </li>                           
+                            <li className="scroll dropdown group/menu">
+                                <a href="javaScript:void();" className="dropdown-toggle"><span>Services</span></a>
+                                <ul className="dropdown-menu hidden group-hover/menu:block">
+                                    {services.services.map((item, i) => (
+                                        <li key={i}> <a href={item.href}> {item.title} </a></li>
+                                    ))}
+                                </ul>
+                            </li>
+                            <li className="scroll dropdown group/menu">
+                                <a href="javaScript:void();" className="dropdown-toggle"><span>Career</span></a>
+                            </li>
+                            <li className="scroll dropdown group/menu">
+                                <a href="javaScript:void();" className="dropdown-toggle"><span>Contact Us</span></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
     </header>
 }
 export default Header
