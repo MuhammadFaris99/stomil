8b9bd5e372d3fb9476f7bb0e74c2018a13891ee7
Alan Aprianto <alan.aprianto@gmail.com>
2023-11-01 10:57:44 +0700

    fix: header shor

--- src/app/components/RunningText.tsx
+++ src/app/components/RunningText.tsx
@@ -8,7 +8,7 @@ function RunningText(){
     return (
         <div className="container relative">
             <div className="grid grid-cols-1 mt-12">
-                <h4 className="text-white lg:text-5xl text-4xl lg:leading-normal leading-normal font-medium mb-7 position-relative">Providing Brilliant Ideas <br />For Your <Typewriter words={words} /></h4>
+                <h4 className="text-white lg:text-5xl text-4xl lg:leading-normal leading-normal font-medium mb-7 position-relative">Providing Brilliant Ideas <br />For Your <Typewriter loop words={words} /></h4>
 
                 <p className="text-white opacity-50 mb-0 max-w-2xl text-lg">Launch your campaign and benefit from our expertise on designing and managing conversion centered Tailwind CSS html page.</p>
 
